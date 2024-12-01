b2dc63ee5fb2e57a392308d1bbd7168582256cff
mfarhann10 <mfarhann1009@gmail.com>
2024-12-02 00:35:03 +0700

    make a reusable StarRating Comp

--- src/components/Star/StarRating.jsx
+++ src/components/Star/StarRating.jsx
@@ -12,20 +12,31 @@ const starContainerStle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
-function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  messages = [],
+  className = "",
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    size: `${size / 1.5}px`,
+  };
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -34,10 +45,16 @@ function StarRating({ maxRating = 5 }) {
             onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import StarRating from "./components/Star/StarRating.jsx";
 import "./index.css";
 // import "../public/css/styles.css";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was {movieRating} Rating</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Good", "Wow", "Amazing"]}
+      className="Test"
+    />
+    <StarRating color="red" size={24} defaultRating={3} />
+    <Test />
   </StrictMode>
 );
